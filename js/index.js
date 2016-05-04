a163700aafb9417094e03ea8fad526abf67b46b6
曲扬波 <quyangbo@hotmail.com>
2016-05-04 15:13:25 +0800

    添加内容(测试展示)

--- js/index.js
+++ js/index.js
@@ -1,3 +1,24 @@
 // I am going to continue to improve this...
 // I attempt to perform candle again but could not overcome the previous version.
 // http://codepen.io/fixcl/pen/nKFDr
+var interval = 1000; 
+function ShowCountDown(year,month,day,divname) 
+{ 
+var now = new Date(); 
+var endDate = new Date(year, month-1, day); 
+var leftTime=endDate.getTime()-now.getTime(); 
+var leftsecond = parseInt(leftTime/1000); 
+//var day1=parseInt(leftsecond/(24*60*60*6)); 
+var day1=Math.floor(leftsecond/(60*60*24)); 
+var hour=Math.floor((leftsecond-day1*24*60*60)/3600); 
+if(hour>0){
+    day1 = day1+1;
+}
+var minute=Math.floor((leftsecond-day1*24*60*60-hour*3600)/60); 
+var second=Math.floor(leftsecond-day1*24*60*60-hour*3600-minute*60); 
+var cc = document.getElementById(divname); 
+if(day1>0){
+cc.innerHTML = "距离"+year+"年"+month+"月"+day+"日还有："+day1+"天";     
+}
+} 
+window.setInterval(function(){ShowCountDown(2016,5,20,'divdown1');}, interval); 
